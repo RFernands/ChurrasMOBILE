6dd5e8b495862e29f361a15787e67d7a8a016047
RFernands <rebecca.fernands@gmail.com>
2018-10-17 16:27:30 -0300

    splash

---
+++ app/src/main/java/net/catito/churrascore/SplashScreenActivity.kt
@@ -0,0 +1,24 @@
+package net.catito.churrascore
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import kotlinx.android.synthetic.main.activity_splash_screen.*
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+        GlideApp.with( this)
+        .load( "https://cdn3.iconfinder.com/data/icons/4th-of-july-1/64/Grill-512.png")
+        .into(imgLogoApp)
+
+        Handler().postDelayed({
+            val inicio = Intent (this, MainActivity::class.java)
+            startActivity(inicio)
+            finish()
+        }, 2000)
+    }
+}
